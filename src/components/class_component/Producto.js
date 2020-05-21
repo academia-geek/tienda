c1795cd1cab18a3438f0011e95b6d625144d23ae
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-05-21 11:37:20 -0500

    cambios

--- src/components/class_component/Producto.js
+++ src/components/class_component/Producto.js
@@ -6,7 +6,8 @@ class Producto extends Component{
         super();
         this.state = {
             codigo : "A000", 
-            precio : 100
+            precio : 100, 
+            parimpar : ""
         }
         this.contador = 0;
     }
@@ -17,9 +18,18 @@ class Producto extends Component{
         
     }
     
+    handleCambioParImpar = (e) => {
+        console.log();
+        if(e.target.value % 2 === 0){
+            this.setState({parimpar: `${e.target.value} es par.`});
+        }else{
+            this.setState({parimpar: `${e.target.value} es impar.`})
+        }
+    }
+    
     render(){
         const {referencia} = this.props;
-        const {codigo,precio} = this.state;
+        const {codigo,precio, parimpar} = this.state;
         console.log("============")
         console.log(this.state);
         console.log("============")
@@ -38,8 +48,18 @@ class Producto extends Component{
                     <div className = "col-lg-6 col-6">Importado: </div>
                 </div>
                 <div className = "row">
-                    <div className = "col-lg-12 col-12">
+                    <div className = "col-lg-6 col-6">
                         <button onClick = {this.handleCambioCodigo} className = "btn btn-warning">Cambiar Código</button>
+                    </div>
+                    <div className = "col-lg-6 col-6">
+                        <div className = "row">
+                            <div className = "col-lg-7 col-7">
+                                <input className = "form-control" type = "number" id = "parimpar" onChange = {this.handleCambioParImpar} />
+                            </div>
+                            <div className = "col-lg-5 col-5">
+                                {parimpar}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
