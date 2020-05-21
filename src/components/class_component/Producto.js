d08fb95612656b768b628824f3222273c1d7812f
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-05-21 10:01:14 -0500

    Trabajo con state

--- src/components/class_component/Producto.js
+++ src/components/class_component/Producto.js
@@ -2,8 +2,20 @@ import React, {Component} from 'react';
 import proptype from 'prop-types';
 
 class Producto extends Component{
+	constructor(){
+        super();
+        this.state = {
+            codigo : "0000"
+        }
+    }
+    
+    handleCambioCodigo(e){
+        console.log("Se realizo el cambio");
+    }
+    
     render(){
         const {referencia} = this.props;
+        console.log(this);
         return(
             <section className = "card bg-success">
             <div className="card-header">
@@ -12,11 +24,16 @@ class Producto extends Component{
             <div className="card-body">
                 <div className = "row">
                     <div className = "col-lg-6 col-6">Referencia:{referencia}</div>
-                    <div className = "col-lg-6 col-6">Código:</div>
+                    <div className = "col-lg-6 col-6">Código: {this.state.codigo}</div>
                 </div>
                 <div className = "row">
                     <div className = "col-lg-6 col-6">Precio: </div>
                     <div className = "col-lg-6 col-6">Importado: </div>
+                </div>
+                <div className = "row">
+                    <div className = "col-lg-12 col-12">
+                        <button onClick = {this.handleCambioCodigo} className = "btn btn-warning">Cambiar Código</button>
+                    </div>
                 </div>
             </div>
         </section>
